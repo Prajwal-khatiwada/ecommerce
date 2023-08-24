7b3d7232c5c3b33e09826cde6274c480208183b5
Prajwal <prazwolkhatiwada@gmail.com>
2023-08-24 10:25:00 +0545

    TODO

--- models/models.go
+++ models/models.go
@@ -11,7 +11,7 @@ type User struct {
 	First_Name      *string            `json:"first_name" validate:"required,min=2,max=30"`
 	Last_Name       *string            `json:"last_name"  validate:"required,min=2,max=30"`
 	Password        *string            `json:"password"   validate:"required,min=6"`
-	Email           *string            `json:"email"      validate:"email,required"`
+	Email           *string            `json:"email"      validate:"email,required"` //email regex API
 	Phone           *string            `json:"phone"      validate:"required"`
 	Token           *string            `json:"token"`
 	Refresh_Token   *string            `josn:"refresh_token"`
